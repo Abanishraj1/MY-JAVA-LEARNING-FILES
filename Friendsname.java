aeeb823cbdbda4a00dd28e299b77a272958441e0
Abanishraj1 <118214356+Abanishraj1@users.noreply.github.com>
2022-11-16 01:17:23 +0530

    Add files via upload

---
+++ Friendsname.java
@@ -0,0 +1,10 @@
+public class Friendsname
+{
+    public static void main(String [] args)
+    {
+        System.out.println("Abhishek");
+        System.out.println("Saurav");
+        System.out.println("Pranav");
+        System.out.println("Subham");
+    }
+}
---
+++ Sum.java
@@ -0,0 +1,11 @@
+public class Sum
+{
+    public static void main(String[] args)
+    {
+        System.out.println("sum of two number");
+        int num1 = 6;
+        int num2 = 7;
+        int sum = num1 + num2;
+        System.out.println(sum);
+    }
+}
